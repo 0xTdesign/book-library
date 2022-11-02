a0c22754f350fc31cd85aa8f7e5793489e6a627a
jimbobdxb <tallisondxb@gmail.com>
2022-11-02 15:08:54 +0400

    updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 const LIVE_API_URL = "https://0xtdesign-book-safe-api.netlify.app/.netlify/functions/api";
-const DEV_API_URL = "http://localhost:8080";
+const DEV_API_URL = "http://localhost:9000/.netlify/functions/api";
 
 export const API_URL = LIVE_API_URL;
